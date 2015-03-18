302490f1190f34492c34da2ca5e20138fc42ef42
Mark Gable <mgable@marketshare.com>
2015-03-18 14:58:42 -0700

    added new seventh question

--- test-questions.js
+++ test-questions.js
@@ -1,6 +1,6 @@
 // Please save all your work in https://jsfiddle.net/ 
 // Return the jsfiddle.net link to your work
-// Answer five out of the six questions
+// Answer five out of the seven questions
 // Use test data when provided, however your functions should work with any similiarly structured data
 // Use any additional libraries you like
 // If you have any questions about this test, please email mgable@marketshare.com
@@ -8,8 +8,7 @@
 // 1: Write a function that searches collection "projects" to find the object with the lowest "e" value and return that object's "i" value.
 var projects = [{i:1, e:41}, {i:5, e:4}, {i:3, e:2}, {i:1, e:90}, {i:5, e:40}, {i:3, e:20}];
 
-// 2: Write a HTML layout which has three columns which stretch 100% of the height of the browser, where the left and right column are 200px wide and the center column fills the leftover width.
-
+// 2: Write a HTML layout which has three columns which stretch 100% of the height of the browser, where the left and right column are 200px wide and the center column fills the leftover width. See example here - https://raw.githubusercontent.com/mgable/test-questions/master/question-2.jpg
 
 // 3: write a function which takes an array of words and a string and finds all occurances of each array word in the string and returns that data as a collection.
 // findWords(["cat", "at"], "Of the many cats in the catacombs, only the black cat and the white cat where at bat") // returns [{"cat": 2}, {"at": 1}]
@@ -23,19 +22,44 @@ var data = [[[1,2,3,4,5],[[1],[2],[3],[4],[5],[6],[[1]]],[9,8,7,6,5,[1,2,3]]],[1
 // makeObject("one.two.three", "foobar")  // returns {one: {two: {three: "foobar"}}}
 // makeObject("kingdom.phylum.class.order", "primates")  // returns {kingdom: {phylum: {class: {order: "primates"}}}}
 
-// 6: when sorting an array of names and acronyms, all of the acronyms are listed at the top. Please make it so sorting an array puts all items in the correct alphabetal order
-var data = [
-	'ACL',
-	'accelerated graphics port',
-	'ATM',
-	'Address Resolution Protocol',
-	'American Standard Code For Information Interchange',
-	'Active Server Page',
-	'ASP'
-]
+// 6: Write a HTML layout which has three nested div's which are all different widths and heights but are all horizonally and vertically centered within each other and then centered vertically and horizonally within the body. See example here - https://raw.githubusercontent.com/mgable/test-questions/master/question-6.jpg
 
-console.info(data.sort()) //["ACL", "ASP", "ATM", "Active Server Page", "Address Resolution Protocol", "American Standard Code For Information Interchange", "accelerated graphics port"]
+// 7: write a function that arranges collection "scenarios" by "createdOn" in descending order (most current first) and outputs the results.
 
-//What we want the return to be is ["accelerated graphics port", "ACL", "Active Server Page", "Address Resolution Protocol", "American Standard Code For Information Interchange", "ASP", "ATM"]
-
+var scenarios = [{
+  "id":3,
+   "name": "MASTER PROJECT",
+   "description": "MASTER PROJECT",
+   "auditInfo": {
+     "createdOn": "March 10, 2014",
+     "createdBy": {
+       "uuid": "system",
+       "name": "me"
+     }
+   }
+ },
+ {
+   "id":1,
+   "name": "Test calculation",
+   "description": "Test calculation",
+   "auditInfo": {
+     "createdOn": "October 1, 2011",
+     "createdBy": {
+       "uuid": "system",
+       "name": "me"
+     }
+   }
+ },
+ {
+   "id": 2,
+   "name": "My Renamed Project - 1426026460724",
+   "description": "This is my new description - 1426026460750",
+   "auditInfo": {
+     "createdOn": "June 15, 2013",
+     "createdBy": {
+       "uuid": "system",
+       "name": "me"
+     }
+   }
+ }]
 
